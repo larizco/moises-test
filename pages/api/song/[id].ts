f318fe4f2a7e8086e90d75d46491031ab2bc9589
Lariz <laris.santana09@gmail.com>
2024-08-18 20:54:15 -0300

    chore: remove console

--- pages/api/song/[id].ts
+++ pages/api/song/[id].ts
@@ -3,12 +3,9 @@ import payload from '../payload.json';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { id } = req.query;
-  console.log(id)
   
   if (req.method === 'GET') {
     const song = payload?.songs?.find((song: any) => song.id == id);
-    console.log(id)
-    console.log(song)
     res.status(200).json(song);
   } else {
     res.status(405).end();
